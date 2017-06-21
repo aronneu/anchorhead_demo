cf87244fe992726430720004ef54680a3bc16584
aronneu <aronneu@users.noreply.github.com>
2017-06-20 20:03:53 -0500

    modified introductory comment in code

--- anchorhead_demo.py
+++ anchorhead_demo.py
@@ -1,11 +1,11 @@
 # This file is basically a short demo of the game Anchorhead, by Michael Gentry.
 #   The text used is almost entirely taken from Anchorhead. The original game
-#   was made using Inform v6.15 (Library 6/7), and my demo version is intended
-#   for Python 2.7. I created this file for Exercise 36 of Learn Python the Hard
+#   was made using Inform v6.15 (Library 6/7), and my demo version was made in
+#   Python 2.7. I created this file for Exercise 36 of Learn Python the Hard
 #   Way, the goal of which is to make a game using the skills learned up to that
 #   point of the book. I thought the exercise would be much more interesting if
 #   I were to mimic a game I really enjoyed. For reference, the full game can be
-#   found at http://pr-if.org/play/anchorhead/ (at least as of this writing.
+#   found at http://pr-if.org/play/anchorhead/ (at least as of this writing).
 #                       -- Aron Neu, 6/11/2017
 
 # Import modules
